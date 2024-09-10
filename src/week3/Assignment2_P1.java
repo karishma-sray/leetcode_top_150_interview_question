409d753d13ba26d5e731560d97dcb74f923538b0
karishma.sray <ksamantaray2109@gmail.com>
2024-09-10 23:24:36 +0530

    check update

--- src/week3/Assignment2_P1.java
+++ src/week3/Assignment2_P1.java
@@ -6,7 +6,7 @@ package week3;
 import java.util.Scanner;
 
 public class Assignment2_P1 {
-
+//recommit
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the array size: ");
