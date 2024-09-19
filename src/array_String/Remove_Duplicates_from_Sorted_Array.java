1007608fbcd5d5f5244a414d0ff308bb4423b701
karishma.sray <ksamantaray2109@gmail.com>
2024-09-19 11:59:18 +0530

    sorted Array

--- src/array_String/Remove_Duplicates_from_Sorted_Array.java
+++ src/array_String/Remove_Duplicates_from_Sorted_Array.java
@@ -57,18 +57,22 @@ public class Remove_Duplicates_from_Sorted_Array {
 	}
 	 public static int removeDuplicates(int[] nums) {
 		
-//	       int j = 0;
-//	       
-//	    	   for(int i = 1; i<nums.length; i++) {
-//	    		   
-//	 	    	  if(nums[i] != nums[j]) {
-//	 	    		  j++;
-//	 	    		  nums[j] = nums[i];
-//	 	    		 
-// 	    	  }
-//	 	       }    		  
-//	       
-//	    	   return j+1;
+	       int j = 0;
+	       
+	    	   for(int i = 1; i<nums.length; i++) {
+	    		   
+	 	    	  if(nums[i] != nums[j]) {
+	 	    		  j++;
+	 	    		  nums[j] = nums[i];
+	 	    		 
+	    	  }
+	 	       }    		  
+	       
+	    	   return j+1;
+	}
+	public static int removeDuplicates1(int[] nums) {
+		
+
 		 int k = 1;
 		 for(int i = 1; i < nums.length; i++) {
 			 if(nums[i-1] != nums[i]) {
