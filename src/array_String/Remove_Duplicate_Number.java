ef44aae321e5f45bd2a54448357c8b012f9cb339
karishma.sray <ksamantaray2109@gmail.com>
2024-09-24 23:53:34 +0530

    array

---
+++ src/array_String/Remove_Duplicate_Number.java
@@ -0,0 +1,37 @@
+package array_String;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Remove_Duplicate_Number {
+
+	public static void main(String[] args) {
+		int num = 5672764;
+		int ans = removeDuplicate(num);
+		System.out.println(ans);
+		
+	}
+
+	public static int removeDuplicate(int num) {
+		//convert integer to string
+		String snum = String.valueOf(num);
+		StringBuffer sb = new StringBuffer();
+		char[] cnum = snum.toCharArray();
+		Set<Character> set = new LinkedHashSet<>();
+		//Remove duplicate
+		for(char c : cnum) {
+			set.add(c);
+		}
+		for(char e : set) {
+			sb.append(e);
+		}
+		// Step 1: Convert StringBuffer to String
+		String ansSNum = sb.toString();
+
+		// Step 2: Convert String to Integer
+		Integer ans = Integer.valueOf(ansSNum);
+		return ans;
+	}
+
+
+}
